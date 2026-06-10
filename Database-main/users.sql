1e007f78546c5747de8a09414bbe81caf51dbcb3
Kislay Sinha <kislayamansinha@gmail.com>
2026-06-10 20:56:32 +0530

    Add files via upload

---
+++ Database-main/users.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+
+    password_hash TEXT NOT NULL,
+
+    role VARCHAR(20) DEFAULT 'user'
+    CHECK(role IN ('user','admin')),
+
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
